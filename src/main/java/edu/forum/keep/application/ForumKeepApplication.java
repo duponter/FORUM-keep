b26b92be4dfb049f062ba3394bfdca897ad76783
Erwin Dupont <erwin.dupont@aca-it.be>
2018-06-14 09:36:16 +0200

    [FORUM-keep-1] Initial Spring Boot Setup with Session, Jersey (JAX-RS), H2, Liquibase, Actuator, Web and Security

---
+++ src/main/java/edu/forum/keep/application/ForumKeepApplication.java
@@ -0,0 +1,12 @@
+package edu.forum.keep.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumKeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumKeepApplication.class, args);
+	}
+}
